e00184ace89e77c09b0be8c40a4cced0fc427fac
Jann Mueller <jann.mueller@iohk.io>
2018-10-30 14:59:16 +0100

    core-to-plc: Bug in LiftPlc

--- plutus-th/test/Spec.hs
+++ plutus-th/test/Spec.hs
@@ -1,22 +1,29 @@
 {-# LANGUAGE DataKinds           #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE ViewPatterns        #-}
 -- remove when we can use addCorePlugin
-{-# OPTIONS -fplugin=Language.Plutus.CoreToPLC.Plugin #-}
+{-# OPTIONS -fplugin=Language.Plutus.CoreToPLC.Plugin -fplugin-opt Language.Plutus.CoreToPLC.Plugin:dont-typecheck #-}
 
 -- | Most tests for this functionality are in the plugin package, this is mainly just checking that the wiring machinery
 -- works.
 module Main (main) where
 
 import           Common
+import           Data.Maybe                               (isJust)
 
 import           TestTH
 
 import           Language.Plutus.TH
 
-import qualified Language.PlutusCore.Pretty as PLC
+import           Language.Plutus.CoreToPLC.Plugin         (getAst, lifted)
+import           Language.PlutusCore                      (applyProgram)
+import           Language.PlutusCore.Evaluation.CkMachine (runCk)
+import           Language.PlutusCore.Evaluation.Result    (evaluationResultToMaybe)
+import qualified Language.PlutusCore.Pretty               as PLC
 
 import           Test.Tasty
+import           Test.Tasty.HUnit
 
 main :: IO ()
 main = defaultMain $ runTestNestedIn ["test"] tests
@@ -29,6 +36,10 @@ tests = testGroup "plutus-th" <$> sequence [
     golden "simple" simple
     , golden "power" powerPlc
     , golden "and" andPlc
+    , pure $ testGroup "LiftPlc" [
+          testCase "Should check signatures in lifted value" (testCount (lifted [Signature 1]) @?= True)
+        , testCase "Should check signatures in TH value" (testCount sigs' @?= True)
+    ]
   ]
 
 simple :: PlcCode
@@ -40,3 +51,12 @@ powerPlc = $(plutus [| $(power (4::Int)) |])
 
 andPlc :: PlcCode
 andPlc = $(plutus [| $(andTH) True False |])
+
+sigs' :: PlcCode
+sigs' = $(plutus [| $(sigs) |])
+
+testCount :: PlcCode -> Bool
+testCount (getAst -> inp) = isOk where
+    isOk = isJust $ evaluationResultToMaybe $ runCk prog
+    prog = count `applyProgram` inp
+    count = getAst $(plutus [| $(TestTH.checkTxInSigs) |])
--- plutus-th/test/TestTH.hs
+++ plutus-th/test/TestTH.hs
@@ -1,10 +1,24 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TemplateHaskell            #-}
 module TestTH where
 
+import           GHC.Generics         (Generic)
 import           Language.Haskell.TH
-
+import           Language.Plutus.Lift (LiftPlc (..), TypeablePlc (..))
 {-# ANN module "HLint: ignore" #-}
+
+newtype Signature = Signature { getSignature :: Int }
+    deriving (Eq, Ord, Show)
+    deriving stock (Generic)
+    -- NOTE: Commenting out the following line,
+    --       and uncommenting the one after that
+    --       will make the error go away!
+    deriving newtype (LiftPlc)
+-- instance LiftPlc Signature
+instance TypeablePlc Signature
 
 power :: Int -> Q Exp
 power n =
@@ -17,3 +31,30 @@ power n =
 
 andTH :: Q Exp
 andTH = [|\(a :: Bool) -> \(b::Bool) -> if a then if b then True else False else False|]
+
+
+checkTxInSigs :: Q Exp
+checkTxInSigs =  [|
+
+    -- Check if all signature correspond to the key `10`
+    \(inp :: [Signature]) ->
+        let
+
+            infixr 3 &&
+            (&&) :: Bool -> Bool -> Bool
+            (&&) l r = if l then r else False
+
+            checkSigs :: [Signature] -> Bool
+            checkSigs l =
+                case l of
+                    (x::Signature):(xs::[Signature]) ->
+                        case x of
+                            Signature i -> i == 10 && checkSigs xs
+                    ([]::[Signature]) -> True
+
+        in
+            checkSigs inp
+    |]
+
+sigs :: Q Exp
+sigs = [| Signature 5 : ([]::[Signature]) |]
