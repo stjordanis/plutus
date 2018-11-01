192fe53af26ef2de26a2d0d40d1a76375bbbdd23
David Smith <shmish111@gmail.com>
2018-11-01 11:07:18 +0000

    bazelified, all tests pass

--- lib.nix
+++ lib.nix
@@ -34,6 +34,7 @@ let
     "plutus-tx"
     "plutus-use-cases"
     "wallet-api"
+    "bazel-runfiles"
   ];
 
   plutusHaskellPkgList = lib.filter (v: v != "plutus-playground-client" && v != "plutus-server-invoker") plutusPkgList;
